509e5de329128fed12ef36788eb7411c64c4a37e
Tatiana Ermakova <tania.ermakova645@gmail.com>
2022-11-23 18:22:31 +0300

    Lesson3

---
+++ Lesson3/lesson3_5.py
@@ -0,0 +1,7 @@
+z = 60
+x = int(input ('Enter Введите число: '))
+while x != z:
+    print('Вы не угадали число')
+    x = int(input('Enter Введите другое число: '))
+else:
+     print('Вы угадали число')

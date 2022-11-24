61a7c2e3ef410c6df4b8d149310407e3a6579c02
Tatiana Ermakova <tania.ermakova645@gmail.com>
2022-11-24 20:51:53 +0300

    replased

--- Lesson3/Lesson3_3.py
+++ Lesson3/Lesson3_3.py
@@ -1,13 +1,13 @@
 while True:
     firstname = input('Имя пользователя: ')
-    age = int(input('Введите ваш возраст: '))
-    if not isinstance(age,int) or age<=0:
+    age = input('Введите ваш возраст: ')
+    if isinstance(age, int) or int(age) <= 0:
         print('Ошибка,повторите ввод')
-    elif age>0 and age<10:
+    elif int(age) > 0 and int(age) < 10:
         print(f'Привет,шкет{firstname}')
-    elif age>=10 and age <=18:
+    elif int(age) >= 10 and int(age) <= 18:
         print(f'Как жизнь,{firstname}?')
-    elif age>18 and age <100:
+    elif int(age) > 18 and int(age) < 100:
         print(f'Что желаете,{firstname}?')
     else:
         print(f'{firstname} вы лжете-в наше время столько не живут...')

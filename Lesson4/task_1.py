d5162ae39edad1b441ddcae75cb141f60b2d141c
Tatiana Ermakova <tania.ermakova645@gmail.com>
2022-12-06 00:28:48 +0300

    Lesson 4: task_1 fix

--- Lesson4/task_1.py
+++ Lesson4/task_1.py
@@ -1,12 +1,13 @@
 source = [1, 2, 3, 4, 5]
 
 
-def recursive_reverse(list_to_reverse: list):
-    if len(list_to_reverse) == 0:
-        return []
+def recursive_reverse(list_to_reverse: list) -> list:
+    if len(list_to_reverse) == 1:
+        return list_to_reverse
     else:
         reversed_part_list = recursive_reverse(list_to_reverse[1:])
-        return reversed_part_list + [list_to_reverse[0]]
+        reversed_part_list.append(list_to_reverse[0])
+        return reversed_part_list
 
 
 print(recursive_reverse(source))
